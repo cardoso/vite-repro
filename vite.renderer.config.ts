d0e08b29bb822159a5f512adef0bb3da8cd3bd55
Matheus Cardoso <matheus@cardo.so>
2024-10-11 20:12:22 -0300

    fix lwc integration

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,13 +1,75 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type Plugin } from 'vite';
 
-import lwc from '@lwc/rollup-plugin';
+import lwc, { type RollupLwcOptions } from '@lwc/rollup-plugin';
+import path from 'node:path';
+
+function disableCssPlugin(): Plugin {
+  return {
+    name: 'disable-css-plugin',
+    enforce: 'pre',
+    configResolved(config) {
+      const cssPlugin = config.plugins.find((plugin) => plugin.name === 'vite:css');
+      cssPlugin.transform = function () {
+        return;
+      }
+
+      const postcssPlugin = config.plugins.find((plugin) => plugin.name === 'vite:css-post');
+      postcssPlugin.transform = function () {
+        return;
+      }
+    }
+  };
+}
+
+function viteLwcPlugin(
+  options?: RollupLwcOptions
+): Plugin {
+  const rollupPlugin = lwc(options);
+
+  return {
+    name: 'vite-plugin-lwc',
+    enforce: 'post',
+    buildStart(options) {
+      // @ts-expect-error - `buildStart` is optional
+      return rollupPlugin.buildStart.call(this, options);
+    },
+    resolveId(source, importer, options) {
+      if (options.isEntry) {
+        return;
+      }
+      // @ts-expect-error - `resolveId` is optional
+      return rollupPlugin.resolveId.call(this, source, importer, options);
+    },
+    load(id) {
+      if (id === path.resolve(__dirname, 'index.html')) {
+        return;
+      }
+
+      // @ts-expect-error - `load` is optional
+      return rollupPlugin.load.call(this, id);
+    },
+    transform(code, id) {
+      if (id === '@lwc/resources/empty_css.css') {
+        return 'export default "";';
+      }
+
+      // @ts-expect-error - `transform` is optional
+      return rollupPlugin.transform.call(this, code, id);
+    },
+  };
+}
 
 // https://vitejs.dev/config
 export default defineConfig({
   plugins: [
-        lwc({
-                modules: [{ dir: 'src/modules' }],
-                include: /src\/modules/
-        }),
-  ]
+    disableCssPlugin(),
+    viteLwcPlugin({
+      rootDir: 'src',
+      modules: [
+        {
+          dir: 'modules',
+        }
+      ],
+      include: ['src/**/*']
+    })],
 });
